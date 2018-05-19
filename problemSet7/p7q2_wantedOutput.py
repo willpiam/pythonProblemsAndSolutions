1611b8eff44c76f679940659aac94a1840521d7a
willpiam <you@example.com>
2018-05-19 17:15:54 -0400

    adding new file which is what i am currently calling the final product. that may change though

---
+++ problemSet7/p7q2_wantedOutput.py
@@ -0,0 +1,342 @@
+"""
+start: 04052018
+Programmer: William
+P7Q1_game.py
+
+I'm going to try using Object orianted programing
+1.
+Be divided into functions
+2.
+Use appropriate data structures (lists) and appropriate internal documentation
+3.
+Be text menu driven (simplified using capital letters or numbers). 
+4.
+Ask separately for the character's first name, then last name
+5.
+Randomly generate a class and race
+6.
+Generate a random number between 5 - 20 per stat. 
+a.
+Character Stats: Strength, Intelligence, Wisdom, Charisma
+7.
+Randomly generate a weapon for the character's 'Back Slot' and 'Belt Slot'
+8.
+Randomly generate an item (Miscellaneous item, Magical item) per hand
+9.
+Randomly generate 8 items (Miscellaneous items, Magical potions, Loot, Stuff that keeps you alive, Magical 
+items) for the pack.
+10.
+Allow the user to read from the file called character.txt to display the previous character
+11.
+Allow the user to create a new character and write to the file character.t
+
+"""
+import pybrary
+import random
+import time
+
+class person(object):
+    def __init__(self, name, typ, race, strength, intel, wisdom, charisma, pack_list, back_slot = "Empty", belt_slot = "EMPTY", r_hand = "EMPTY", l_hand = "EMPTY" ):
+        self.name = name #makes objects name
+        self.typ = typ #type used instead of class because class is a reserved word
+        self.race = race
+        self.strength = strength
+        self.intel = intel
+        self.wisdom = wisdom
+        self.charisma = charisma
+        self.pack_list = pack_list
+        self.back_slot = back_slot
+        self.belt_slot = belt_slot
+        self.r_hand = r_hand
+        self.l_hand = l_hand
+           
+    def update_file(self):#updates file holding charecters details
+        pybrary.writeArray(self.name+".txt", [self.name, self.typ, self.race, str(self.strength), str(self.intel), str(self.wisdom), str(self.charisma), str(self.pack_list), self.back_slot, self.belt_slot, self.r_hand, self.l_hand])
+        
+    #print out stuff
+    def __str__(self):#makes it so when I print a person it prints actual useful information
+        VALUE = """
+ADVENTURES NAME: """+self.name+"""
+CLASS: """+self.typ+"""
+RACE: """+self.race+"""
+STRENGTH: """+self.strength+"""
+INTELLIGENCE: """+self.intel+"""
+WISDOM: """+self.wisdom+"""
+CHARISMA: """+self.charisma+"""
+BACK SLOT: """+self.back_slot+"""
+BELT SLOT: """+self.belt_slot+"""
+RIGHT HAND: """+self.r_hand+"""
+LEFT HAND: """+self.l_hand+"""
+PACK: """ +self.pack_list+"""
+
+
+"""
+        return VALUE
+
+def make_from_file(fileName):
+    array = pybrary.readFile(fileName)
+    #print (array)
+    player = person(array[0], array[1], array[2], array[3], array[4], array[5], array[6], array[7], array[8], array[9], array[10], array[11])
+    return player
+
+def get_first_name():#creates frist name from user input
+    return input("ENTER CHARECTERS FIRST NAME-->")
+def get_last_name():#creates last name from user input
+    return input("ENTER CHARECTERS LAST NAME-->")
+
+def ran_wep():#makes random weapon
+    num = random.randint(0,8)
+    wep = ""
+    if num == 0:
+        wep = "SMALL SWORD"
+    elif num == 1:
+        wep = "MEDIUM SWORD"
+    elif num == 2:
+        wep = "LARGE SWORD"
+    elif num == 3:
+        wep = "SMALL AXE"
+    elif num == 4:
+        wep = "MEDIUM AXE"
+    elif num == 5:
+        wep = "LARGE AXE"
+    elif num == 6:
+        wep = "CROSSBOW"
+    elif num == 7:
+        wep = "SHORT BOW"
+    elif num == 8:
+        wep = "LONG BOW"
+    else:
+        print ("something went wrong in ran_wep")
+    return wep
+def ran_class():#makes random class or type of charecter
+    num = random.randint(0,4)
+    typ = ""
+    if num == 0:
+        typ = "FIGHTER"
+    elif num == 1:
+        typ = "WIZARD"
+    elif num == 2:
+        typ = "THIEF"
+    elif num == 3:
+        typ = "HEALER"
+    elif num == 4:
+        typ = "RANGER"
+    else:
+        print ("SOMETHING WENT WRONG IN RAN_CLASS")
+    return typ
+def ran_race():#makes random race of charecter
+    num = random.randint(0,4)
+    race = ""
+    if num == 0:
+        race = "HUMAN"
+    elif num == 1:
+        race = "ELF"
+    elif num == 2:
+        race = "DWARF"
+    elif num == 3:
+        race = "HALFLING"
+    elif num == 4:
+        race = "GNOME"
+    else:
+        print ("SOMETHING WENT WRONG IN RAN_race")
+    return race
+
+def ran_strength():
+    return random.randint(5, 20)
+def ran_intel():
+    return random.randint(5,20)#these 4 functions could be made into one
+def ran_wise():
+    return random.randint(5, 20)
+def ran_charis():
+    return random.randint(5, 20)
+
+def ran_magic_thing():
+    num = random.randint(0,3)
+    thing = ""
+    if (num == 0):
+        thing = "WAND"
+    elif (num == 1):
+        thing = "SCROLL"
+    elif (num == 2):
+        thing = "RING"
+    elif (num == 3):
+        thing = "_BLANK_"
+    return thing
+
+def ran_normal_thing(): #makes a random micellaneous item
+    num = random.randint(0,4)
+    thing = ""
+    if (num == 0):
+        thing = "TORCH"
+    elif (num == 1):
+        thing = "LAMP"
+    elif (num == 2):
+        thing = "KEY"
+    elif (num == 3):
+        thing = "MAP"
+    elif (num == 4):
+        thing = "_BLANK_"
+    return thing
+
+def ran_food():#will either return food or healing potion
+    num = random.randint(0,1)
+    thing = ""
+    if (num == 0):
+        thing = "FOOD"
+    elif (num == 1):
+        thing = "HEALING POTION"
+    return thing
+
+def ran_potion(): #will return a random potion
+    num = random.randint(0, 2)
+    thing = ""
+    if (num == 0):
+        thing = "RED POTION"
+    elif (num == 1):
+        thing = "BLUE POTION"
+    elif (num == 2):
+        thing = "GREEN POTION"
+    return thing
+
+def ran_loot():#returns random loot
+    num = random.randint(0, 2)
+    thing = ""
+    if (num == 0):
+        thing = "GEMS"
+    elif (num == 1):
+        thing = "COINS"
+    elif (num == 2):
+        thing = "ARTIFACT"
+    return thing
+
+def ran_thing(): #randomly choses to ither pick a magic thing or a miscellaneous thing
+    num = random.randint(0,1)#random number between 0 and 1
+    if num == 0:
+        return ran_magic_thing()
+    elif num == 1:
+        return ran_normal_thing()
+    
+def any_ran_thing():#can return a missalaneus thing, magic thing, loot, potions, stuff that keeps you alive
+    num = random.randint(0, 4)
+    thing = ""
+    if (num == 0):
+        thing = ran_normal_thing()
+    elif (num == 1):
+        thing = ran_magic_thing()
+    elif (num == 2):
+        thing = ran_loot()
+    elif (num == 3):
+        thing = ran_potion()
+    elif (num == 4):
+        thing = ran_food()
+    return thing    
+
+def build_from_file(fileName):#program rebuilds a charecter from a file
+    #put each line of file into array
+    array = pybrary.readFile(fileName)#uses library I made
+    name = array[0]
+    typ = array[1]
+    race = array[2]
+    strength = array[3]
+    intel = array[4]
+    wise = array[5]
+    chaisma = array[6]
+    pack = array[7]
+    back = array[8]
+    belt = array[9]
+    r = array[10]
+    l = array[11]
+
+    char = person(name, typ, race, strength, intel, wise, chaisma, pack, back, belt, r, l)
+    return char
+
+def menu():#main menu for user to see... returns True when the user wishes to quit the program
+    print ("""
+
+WHAT WOULD YOU LIKE TO DO?
+
+A.       CREATE A NEW CHARECTER
+
+B.       DISPLAY A CHARECTER
+
+C.       QUIT THE PROGRAM
+
+ENTER YOUR CHOICE BY TYPING THE LETTER CORISPONDING TO WHAT YOU WISH TO DO.
+""")
+    DO = input(">>")
+    DO = DO.upper()
+    if (DO == "C"):
+        print ("Goodbye!")
+        return True#ends the loop found in main and ends the program
+    elif (DO == "A"):
+        print ("Okay lets make a new charecter!")
+        name = get_first_name()
+        name = name+" "+get_last_name()
+        typ = ran_class()
+        race = ran_race()
+        strength = ran_strength()
+        intel = ran_intel()
+        wise = ran_wise()
+        charisma = ran_charis()
+        pack = []
+        for i in range (0, 8):
+            pack.append(any_ran_thing())
+        
+        back = ran_wep()
+        belt = ran_wep()
+        r = ran_thing()
+        l = ran_thing()
+        player = person(name, typ, race, strength, intel, wise, charisma, pack, back, belt, r, l)
+        player.update_file()#puts the charecter into a txt file
+        print ("CHARECTER NAMED "+name.upper()+" HAS BEEN MADE")
+    elif (DO == "B"):
+        print ("""
+PLEASE ENTER THE NAME OF A FILE HOLDING THE DETAILS OF THE CHARECTER YOU WISH TO DISPLAY.
+THESE FILES ARE NAMED IN THIS FORMAT: firstname secondname.txt
+ENTERING A FILE WHICH DOES NOT EXCIST COULD CRASH THIS PROGRAM.
+""")
+        try:
+            fileName = input(">>")
+            player = build_from_file(fileName)
+            print (player)
+        except:
+            print ("YOU ENTERED A FILE WHICH DOES NOT EXCIST.  PLEASE DON'T DO THAT AGAIN")
+            time.sleep(2)#waits for a few seconds to allow the reader a chance to notice the above line before more text is written
+    else:
+        print ("THE INPUT YOU GAVE IS NOT A VALID OPTION. PLEASE READ INSTRUCTIONS AS THEY ARE PROVIDED.")
+        time.sleep(2)
+    return False#must be the very last line in this function
+
+def main():
+    stop = False
+    while (stop == False):
+        stop = menu()
+    
+        """
+    name = get_first_name()
+    name = name+" "+get_last_name()
+    typ = ran_class()
+    race = ran_race()
+    strength = ran_strength()
+    intel = ran_intel()
+    wise = ran_wise()
+    charisma = ran_charis()
+    pack = []
+    for i in range (0, 8):
+        pack.append(any_ran_thing())
+        
+    back = ran_wep()
+    belt = ran_wep()
+    r = ran_thing()
+    l = ran_thing()
+    
+    player1 = person(name, typ, race, strength, intel, wise, charisma, pack, back, belt, r, l)
+    player2 = build_from_file("e e.txt")
+    print (player2)
+    print (player1)
+    player1.update_file()"""
+
+
+
+if __name__ == "__main__":
+    main()
